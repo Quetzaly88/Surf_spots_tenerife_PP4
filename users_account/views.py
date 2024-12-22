5e180153fe8fd9e6e31727466802f6d2103af4f0
Andrea Nilsson <andreaycoral@icloud.com>
2024-12-22 05:53:23 +0000

    Create registration view to render the form and handle registration

--- users_account/views.py
+++ users_account/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+#import modules and classes
+from django.shortcuts import render, redirect #renders HTML templates, redirects to a different URL
+from .forms import RegistrationForm # Custom form class for user registration. 
+from django.contrib import messages # displays messages
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST': 
+        form = RegistrationForm(request.POST)
+        if form_valid(): #check if form is valid, then save data to create user
+            form.save()
+            messages.success(request, 'Your account was successfully created!') #account created, redirect to login
+            return redirect('login')
+    else:
+        form = RegistrationForm() #if GET render empty form
+        
+return render(request, 'users_account/register.html', {'form': form}) #render the registration template and pass the form instance to the template context. 
+
+
