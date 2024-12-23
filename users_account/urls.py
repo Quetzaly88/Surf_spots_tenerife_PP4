dc5207e001aec4b6f34b741ea78316c506fe727c
Andrea Nilsson <andreaycoral@icloud.com>
2024-12-23 23:18:17 +0000

    Add login and logout routes

--- users_account/urls.py
+++ users_account/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'), #route for registration
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
