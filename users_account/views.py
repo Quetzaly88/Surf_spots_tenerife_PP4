fabe5fc7e8ea9295727086cc4f57ab6718ab64ab
Andrea Nilsson <andreaycoral@icloud.com>
2024-12-23 23:04:47 +0000

    add the login view

--- users_account/views.py
+++ users_account/views.py
@@ -2,8 +2,10 @@
 from django.shortcuts import render, redirect #renders HTML templates, redirects to a different URL
 from .forms import RegistrationForm # Custom form class for user registration. 
 from django.contrib import messages # displays messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+    #register view
 def register(request):
     if request.method == 'POST': 
         form = RegistrationForm(request.POST)
@@ -16,4 +18,16 @@ def register(request):
         
     return render(request, 'users_account/register.html', {'form': form}) #render the registration template and pass the form instance to the template context. 
 
-
+    #login view
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")#form
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None: 
+            login(request)
+            messages.success(request, "You are loged in!")
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, 'users_account/login.html')
