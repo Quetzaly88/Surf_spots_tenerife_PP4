b93354160db69832864acfcdb952d93add581aac
Andrea Nilsson <andreaycoral@icloud.com>
2024-12-27 20:56:39 +0000

    add SurfSpot to admin.py

--- users_account/admin.py
+++ users_account/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import NovaUser
+from .models import SurfSpot
 
 # Register your models here.
 admin.site.register(NovaUser)
@@ -7,3 +8,7 @@ admin.site.register(NovaUser)
 @admin.register(SurfSpot)
 class SurfSpotAdmin(admin.ModelAdmin):
     list_display = ('title', 'location', 'user', 'created_at')
+
+@admin.register(SurfSpot)
+class SurfSpotAdmin(admin.ModelAdmin):
+    list_display = ('title', 'location', 'user', 'created_at') 
