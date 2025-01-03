eedcd6068a77c704cf33528eff32dc03458ea228
Andrea Nilsson <andreaycoral@icloud.com>
2025-01-03 21:22:09 +0000

    Add css styling to have a responsive layout. Make small changes on html

--- users_account/urls.py
+++ users_account/urls.py
@@ -7,10 +7,5 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('home/', views.home_view, name='home'), #correct
     path('surf_spots/list/', views.list_surf_spots, name='list_surf_spot'),   
-    #path('', views.home_view, name='home'),
     path('', views.login_view, name='login'), #Avoid no conflicting patterns are overriding home route.
 ]
-
-# path('api/surf_spots/create', views.create_surf_spot, name='create_surf_spot'), 
-#     path('api/surf_spots/list', views.list_surf_spots, name='list_surf_spot'), 
-#     path('list/', views.list_surf_spots, name='list_surf_spot'),
